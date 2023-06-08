bfbb05363ab9eca76c0f5457cecec2d9f1c1bb1b
Muthaura <111274197+MuthauraCodex@users.noreply.github.com>
2023-06-08 03:10:55 +0300

    Update file_storage.py

--- server/database/models/engine /file_storage.py
+++ server/database/models/engine /file_storage.py
@@ -5,17 +5,15 @@ Contains the FileStorage class
 
 import json
 import models
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
+from models.inventory import Inventory
+from models.orders import Orders
+from models.customers import Customers
+from models.reports import Reports
 from models.user import User
 from hashlib import md5
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {"Inventory": Inventory, "Orders": Orders,
+           "Customers": Customers, "Reports": Reports, "User": User}
 
 
 class FileStorage:
